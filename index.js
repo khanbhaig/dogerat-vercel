b0023fc82af395ddb09e96bb4a4134d4d131a1c3
khanbhaig <106430404+khanbhaig@users.noreply.github.com>
2023-12-13 16:08:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = 'your token here'
-const id = 'chat id here'
+const token = '6231959405:AAE9wkZ2PBFM0ZXT4XFHtHhGISAg8oCyREw'
+const id = '6066856746'
 const address = 'https://www.google.com'
 
 const app = express();
